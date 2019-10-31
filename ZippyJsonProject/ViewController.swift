0af6fcdf213af082d2ed35a748c486a4a4fc9262
Noah Gilmore <noah.gilmore@autodesk.com>
2019-10-31 16:58:22 -0700

    Add code demonstrating ZippyJSON bug

--- ZippyJsonProject/ViewController.swift
+++ ZippyJsonProject/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import ZippyJSON
+
+let string = "{\"key\": 123}"
+
+struct Example: Codable {
+    let key: String
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.key = (try? container.decode(String.self, forKey: .key)) ?? ""
+    }
+}
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let decoder = ZippyJSONDecoder()
+        do {
+            let data = string.data(using: .utf8)!
+            let element = try decoder.decode(Example.self, from: data)
+            print(element)
+        } catch {
+            print("Oh no!")
+            print(error)
+        }
     }
 
 
